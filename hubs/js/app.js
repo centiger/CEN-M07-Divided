3999065fae416bf074c6ebd976b404ba0129a622
centiger <centiger2@gmail.com>
2026-06-10 08:38:51 +0900

    Add files via upload

--- hubs/js/app.js
+++ hubs/js/app.js
@@ -4,7 +4,7 @@ const $=id=>document.getElementById(id);
 const params=new URLSearchParams(location.search);
 
 async function loadData(){
-  const res=await fetch('data/hubs.json?v=divided-standard-5',{cache:'no-store'});
+  const res=await fetch('data/hubs.json?v=galmel-standard-2',{cache:'no-store'});
   const data=await res.json();
   HUBS=data.hubs||[];
   const slug=params.get('hub')||data.defaultHub||(HUBS[0]&&HUBS[0].slug);
@@ -54,7 +54,6 @@ function renderHub(slug){
   $('overviewFlow').innerHTML=nodes(current.overviewFlow||[]);
 
   setOptional('eventsSection','events',current.events, renderListOrFlow);
-  setOptional('peopleSection','people',current.people);
   setOptional('meaningSection','meaning',current.meaning);
   setOptional('integrationSection','integration',current.integration);
   setOptional('bibleSection','bible',current.bible);
@@ -107,5 +106,5 @@ $('backBtn').onclick=()=>openUrl('../index.html');
 $('hubListBtn').onclick=()=>$('drawer').classList.add('show');
 $('drawerClose').onclick=()=>$('drawer').classList.remove('show');
 $('drawer').onclick=e=>{if(e.target.id==='drawer')$('drawer').classList.remove('show')};
-if('serviceWorker'in navigator)navigator.serviceWorker.register('./sw.js?v=divided-standard-5').catch(()=>{});
+if('serviceWorker'in navigator)navigator.serviceWorker.register('./sw.js?v=galmel-standard-2').catch(()=>{});
 loadData();
