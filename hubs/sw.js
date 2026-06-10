e732c765025799ee393af47100f820c5a7226b55
centiger <centiger2@gmail.com>
2026-06-10 12:31:10 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,1 +1,2 @@
 const CACHE='cen-divided-galmel-standard-v1';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+// update: jerusalem-fall 1781062205
