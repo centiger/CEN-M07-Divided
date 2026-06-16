f0a98b51a91a742b907ff675de626634721f20bc
centiger <centiger2@gmail.com>
2026-06-16 10:20:09 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,1 +1,2 @@
 const CACHE='cen-divided-hubs-review-20260610';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+/* 20260616-next-era-links */
